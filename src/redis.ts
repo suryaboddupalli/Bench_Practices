4a661837c041857a6d8dec05c2a3b71bb022bc9e
Surya Ravi <surya.ravi@aspiresys.com>
2022-06-02 12:56:54 +0530

    2 June

--- src/redis.ts
+++ src/redis.ts
@@ -4,16 +4,12 @@ import { config } from './convict/config'
 const redisConfig = config.get('redis')
 
 const nodes = redisConfig.server.ports.map((port: any) => {
-    console.log(port)
     const nodes = {
         host: redisConfig.server.host,
         port: port
     }
     return nodes
 })
-
-console.log(nodes)
-
 
 
 export const cluster = new redis.Cluster(nodes);
